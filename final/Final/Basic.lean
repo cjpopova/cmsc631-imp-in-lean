19733f49db67fbaac41d5628fd74cbfa98759ad8
cjpopova <casparpopova2023@u.northwestern.edu>
2024-11-26 14:23:17 -0500

    Finish eval as functions

--- final/Final/Basic.lean
+++ final/Final/Basic.lean
@@ -1,7 +1,7 @@
 import Mathlib.Data.Real.Basic
 
 inductive aexp where
-  | ANum (n : nat)
+  | ANum (n : Nat)
   | APlus (a1 a2 : aexp)
   | AMinus (a1 a2 : aexp)
   | AMult (a1 a2 : aexp)
@@ -23,17 +23,26 @@ open bexp
 #check BTrue
 #check (BEq (ANum 3) (ANum 3))
 
------------------------------------
+------------- Evaluation as functions ----------------------
 
-def aeval (a : aexp) : nat :=
+def aeval (a : aexp) : Nat :=
   match a with
   | ANum n => n
   | APlus  a1 a2 => (aeval a1) + (aeval a2)
   | AMinus a1 a2 => (aeval a1) - (aeval a2)
   | AMult  a1 a2 => (aeval a1) * (aeval a2)
 
+def beval (b : bexp) : Bool :=
+  match b with
+  | BTrue       => true
+  | BFalse      => false
+  | bexp.BEq a1 a2   => (aeval a1) = (aeval a2)
+  | BNeq a1 a2  => ¬ ((aeval a1) = (aeval a2))
+  | BLe a1 a2   => (aeval a1) <= (aeval a2)
+  | BGt a1 a2   => ¬ ((aeval a1) <= (aeval a2))
+  | BNot b1     => ¬ (beval b1)
+  | BAnd b1 b2  =>  (beval b1) ∧ (beval b2)
 
-
-
+---------------- Evaluation as a Relation ----------------------
 
 
